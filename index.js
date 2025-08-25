13affb7c8a0e173fe4cbc92e046079f73dfe237c
Oscar Mochizaki <oymg81@gmail.com>
2025-08-24 21:05:15 -0400

    json

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import GAMES_DATA from './games.js';
 
 // create a list of objects to store the data about the games using JSON.parse
 const GAMES_JSON = JSON.parse(GAMES_DATA)
+window.GAMES_JSON = GAMES_JSON;
+
 
 // remove all child elements from a parent element in the DOM
 function deleteChildElements(parent) {
